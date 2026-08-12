f8ee766ae0c5aa6ffa0cffd92206db16685d18ca
Vũ Hoàng Minh <minhvh.coding@gmail.com>
2026-08-12 12:58:01 +0700

    Add advanced protection and position management settings

--- src/PropRiskManager/State/PluginSettings.cs
+++ src/PropRiskManager/State/PluginSettings.cs
@@ -15,4 +15,15 @@ public sealed class PluginSettings
     public double TakeProfitPips { get; set; } = 40;
     public double MaxRiskPercent { get; set; } = 5.0;
     public double MaxSpreadPips { get; set; }
+
+    public bool TrailingEnabled { get; set; }
+    public TrailingMode TrailingMode { get; set; } = TrailingMode.Custom;
+    public double TrailingPips { get; set; } = 15;
+    public bool BreakEvenEnabled { get; set; }
+    public double BreakEvenTriggerPips { get; set; } = 20;
+    public double BreakEvenOffsetPips { get; set; } = 10;
+
+    public ManagementScope ManagementScope { get; set; } = ManagementScope.CurrentSymbol;
+    public double PartialClosePercent { get; set; } = 50;
+    public double ManualBreakEvenOffsetPips { get; set; } = 10;
 }
