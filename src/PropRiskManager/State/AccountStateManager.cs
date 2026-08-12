29e4e2be07e1a8f1fa21e7cbe0f721db7905b7b0
Vũ Hoàng Minh <minhvh.coding@gmail.com>
2026-08-12 12:15:00 +0700

    Add account runtime rollover and peak tracking

---
+++ src/PropRiskManager/State/AccountStateManager.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace PropRiskManager.State;
+
+public static class AccountStateManager
+{
+    public static AccountRuntimeState Create(int accountNumber, DateTime tradingDay, double balance, double equity)
+    {
+        return new AccountRuntimeState
+        {
+            AccountNumber = accountNumber,
+            TradingDay = tradingDay.Date,
+            DayStartBalance = balance,
+            DayStartEquity = equity,
+            BalancePeak = balance,
+            EquityPeak = equity
+        };
+    }
+
+    public static void Update(AccountRuntimeState state, DateTime tradingDay, double balance, double equity)
+    {
+        var day = tradingDay.Date;
+        if (state.TradingDay != day)
+        {
+            state.TradingDay = day;
+            state.DayStartBalance = balance;
+            state.DayStartEquity = equity;
+        }
+
+        state.BalancePeak = Math.Max(state.BalancePeak, balance);
+        state.EquityPeak = Math.Max(state.EquityPeak, equity);
+    }
+}
